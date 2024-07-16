12a69b51e9727fe0706f579205e77c7d711d9821
TavoGo <ogzzauto@gmail.com>
2024-07-16 15:01:39 -0600

    Button

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const butt1 = document.getElementById('butt1');
+
+//open picture when click button
+function picture(){document.getElementById('pic').removeAttribute('hidden')};
+butt1.addEventListener('click', picture);
