bc892e8c42c6b4630c12c3cf30d0859d88eb6416
Caren Garcia <carenvaleria90@gmail.com>
2024-07-18 16:19:29 -0600

    adding nav

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => {
+    return (
+        <nav>
+        <div class="container">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>
+        </div>
+        </nav>
+    );
+}
+
+
+
+export default Nav;
